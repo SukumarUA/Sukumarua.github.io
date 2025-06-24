d60ca4ddf461622728d99af964a7b4c9b92e0b31
Sukumar <schinthalapudi@albany.edu>
2025-06-23 20:07:56 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sukumarua.github.io/', // ✅ Updated to match your GitHub repo name
+  base: '', // ✅ Updated to match your GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
